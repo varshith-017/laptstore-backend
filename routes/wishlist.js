8ecfd6e33e5df964f3fef29cf1a2dd6d3bbf78a4
varshith-017 <varshithap03@gmail.com>
2026-06-04 22:44:16 +0530

    Update wishlist.js

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,45 +2,59 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Product = require("../models/Product");
 
-// ADD
+// ADD TO WISHLIST
 router.post("/add", async (req, res) => {
-  const { userId, productId } = req.body;
+  try {
+    const { userId, productId } = req.body;
 
-  const user = await User.findById(userId);
+    const user = await User.findById(userId);
 
-  if (!user.wishlist.includes(productId)) {
-    user.wishlist.push(productId);
+    if (!user.wishlist.includes(productId)) {
+      user.wishlist.push(productId);
+      await user.save();
+    }
+
+    res.json({
+      success: true,
+      wishlist: user.wishlist
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-
-  await user.save();
-
-  res.json(user.wishlist);
 });
 
-// GET
+// GET WISHLIST PRODUCTS
 router.get("/:userId", async (req, res) => {
-  const user = await User.findById(req.params.userId);
+  try {
+    const user = await User.findById(req.params.userId);
 
-  const products = await Product.find({
-    _id: { $in: user.wishlist }
-  });
+    const products = await Product.find({
+      _id: { $in: user.wishlist }
+    });
 
-  res.json(products);
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
-// REMOVE
+// REMOVE FROM WISHLIST
 router.post("/remove", async (req, res) => {
-  const { userId, productId } = req.body;
+  try {
+    const { userId, productId } = req.body;
 
-  const user = await User.findById(userId);
+    const user = await User.findById(userId);
 
-  user.wishlist = user.wishlist.filter(
-    (id) => id.toString() !== productId
-  );
+    user.wishlist = user.wishlist.filter(
+      (id) => id.toString() !== productId
+    );
 
-  await user.save();
+    await user.save();
 
-  res.json(user.wishlist);
+    res.json(user.wishlist);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
